af72b7b3f6349f000ba0509ed4e266e07db339d2
Sulyun Lee <sulyun-lee@uiowa.edu>
2019-06-21 14:20:36 -0500

    Modify to write files

--- generate_combined_dataset.py
+++ generate_combined_dataset.py
@@ -1,3 +1,56 @@
 import pandas as pd
 
+if __name__ == "__main__":
+    com_folder = "data/proposed_com/"
+    emb_folder = "data/proposed_emb/"
+    textsim_folder = "data/proposed_textsim/"
+
+    start_week = 50
+    end_week = 101
+
+    for i in range(end_week - start_week - 1):
+    # for i in range(1):
+        print('Generating week {} train and test dataset...'.format(i+start_week))
+        com_train_filename = "{}bax_week{}_train.csv".format(com_folder, i+start_week)
+        emb_train_filename = "{}bax_week{}_train.csv".format(emb_folder, i+start_week)
+        textsim_train_filename = "{}bax_week{}_train.csv".format(textsim_folder, i+start_week)
+
+        com_test_filename = "{}bax_week{}_test.csv".format(com_folder, i+start_week)
+        emb_test_filename = "{}bax_week{}_test.csv".format(emb_folder, i+start_week)
+        textsim_test_filename = "{}bax_week{}_test.csv".format(textsim_folder, i+start_week)
+        
+
+        # Train
+        com_train = pd.read_csv(com_train_filename)
+        emb_train = pd.read_csv(emb_train_filename)
+        textsim_train = pd.read_csv(textsim_train_filename)
+
+        combined_train = pd.concat([com_train, emb_train[['BC_emb','GD_emb','MB_emb','PM_emb']]], axis=1)
+        combined_train = pd.concat([combined_train, textsim_train[['BC_Textsim','GD_Textsim','MB_Textsim']]], axis=1)
+
+        combined_train = combined_train[['pair', 'BC_PA', 'BC_AA', 'BC_JC', 'BC_CM', 'BC_CLP', 'BC_emb', 'BC_Textsim',
+                                        'GD_PA', 'GD_AA', 'GD_JC', 'GD_CM', 'GD_CLP', 'GD_emb', 'GD_Textsim',
+                                        'MB_PA', 'MB_AA', 'MB_JC', 'MB_CM', 'MB_CLP', 'MB_emb', 'MB_Textsim',
+                                        'PM_PA', 'PM_AA', 'PM_JC', 'PM_CM', 'PM_CLP', 'PM_emb', 'label']]
+
+        combined_train.to_csv("data/proposed_cet/bax_week{}_train.csv".format(i+start_week))
+
+        # Test
+        com_test = pd.read_csv(com_test_filename)
+        emb_test = pd.read_csv(emb_test_filename)
+        textsim_test = pd.read_csv(textsim_test_filename)
+
+        combined_test = pd.concat([com_test, emb_test[['BC_emb','GD_emb','MB_emb','PM_emb']]], axis=1)
+        combined_test = pd.concat([combined_test, textsim_test[['BC_Textsim','GD_Textsim','MB_Textsim']]], axis=1)
+
+        combined_test = combined_test[['pair', 'BC_PA', 'BC_AA', 'BC_JC', 'BC_CM', 'BC_CLP', 'BC_emb', 'BC_Textsim',
+                                        'GD_PA', 'GD_AA', 'GD_JC', 'GD_CM', 'GD_CLP', 'GD_emb', 'GD_Textsim',
+                                        'MB_PA', 'MB_AA', 'MB_JC', 'MB_CM', 'MB_CLP', 'MB_emb', 'MB_Textsim',
+                                        'PM_PA', 'PM_AA', 'PM_JC', 'PM_CM', 'PM_CLP', 'PM_emb', 'label']]
+
+        combined_test.to_csv("data/proposed_cet/bax_week{}_test.csv".format(i+start_week))
+
+
+
+
 
